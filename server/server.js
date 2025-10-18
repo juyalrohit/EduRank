a2ce04271ab657bfb7e4dfb515ad523ac7b88bca
juyalrohit <rohitjuyalp205@gmail.com>
2025-10-18 23:30:01 +0530

    Migreate Databases

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 
 
